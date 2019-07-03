e3970090145026d3e5a86deb5828e1f015d53cc5
tetuyoko <tyokoyama53@gmail.com>
2019-07-03 15:06:42 +0900

    feat: eventEmitter

--- src/component.js
+++ src/component.js
@@ -1,9 +1,49 @@
 import Vue from 'vue';
+
+const eventHub = new Vue();
+
 export default Vue.component('button-component', {
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>',
+  template: '<div>\
+               <button @click="doit">\
+                 count: {{ count }} chillCount: {{ chillCount }}.\
+               </button>\
+               <chill />\
+             </div>',
+  created() {
+    eventHub.$on('addCount', (delta) => {
+      this.addCount(delta);
+    });
+  },
+  data: () => {
+    return {
+      count: 0,
+      chillCount: 0
+    }
+  },
+  methods: {
+    addCount(delta) {
+      this.chillCount = this.chillCount + delta;
+    },
+    doit() {
+      this.count++;
+      //eventHub.$emit('addCount', 1);
+    },
+  },
+})
+
+Vue.component('chill', {
+  template: '<button @click="doit">\
+               You clicked me {{ count }} times.\
+            </button>',
   data: () => {
     return {
       count: 0
     }
+  },
+  methods: {
+    doit() {
+      this.count++;
+      eventHub.$emit('addCount', 1);
+    },
   },
 })
--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-//import App from './app.js';
 import './main.css';
-//import './component'
+import './component'
 
-const test =  new Vue({
-    template: '<div>hoge</div>',
-    data () {
-      return {
-        msg: 'Welcome to Your Vue.js App!'
-      }
-    }
-  });
 export default () => {
   new Vue(App).$mount('#app')
 };
